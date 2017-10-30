84dde51423d91c475e7a30af0a584f6869bfaee7
Maksim <maksalmak@gmail.com>
2017-10-30 18:56:57 +0300

    With Cacellation

--- 30 09/ClassWork/ChatClient/Program.cs
+++ 30 09/ClassWork/ChatClient/Program.cs
@@ -12,25 +12,39 @@ namespace ChatClient
         {
             client.Client.ReceiveTimeout = 1000;
             string message;
-            var thread = new Thread(Listen);
+            CancellationTokenSource source = new CancellationTokenSource();
+            var thread = new Thread(() => Listen(source.Token));
             thread.Start();
             IPEndPoint endPoint = new IPEndPoint(IPAddress.Broadcast, 5000);
-            while((message = Console.ReadLine()) != "exit")
+            while ((message = Console.ReadLine()) != "exit")
             {
                 var data = Encoding.UTF8.GetBytes(message);
                 client.Send(data, data.Length, endPoint);
             }
+            source.Cancel();
             System.Console.WriteLine("THE BIG END");
         }
 
-        static void Listen()
+        static void Listen(CancellationToken token)
         {
             while (true)
             {
-                IPEndPoint point = default(IPEndPoint);
-                var data = client.Receive(ref point);
-                var message = Encoding.UTF8.GetString(data);
-                System.Console.WriteLine($"getted {message}");
+                try
+                {
+                    IPEndPoint point = default(IPEndPoint);
+                    var data = client.Receive(ref point);
+                    var message = Encoding.UTF8.GetString(data);
+                    System.Console.WriteLine($"getted {message}");
+                }
+                catch
+                {
+                    if(token.IsCancellationRequested)
+                    {
+                        System.Console.WriteLine("Second thread dead");
+                        return;
+                    }
+                    System.Console.WriteLine("I have exception");
+                }
             }
         }
     }
