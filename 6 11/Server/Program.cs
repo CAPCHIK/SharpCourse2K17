434a2739844b63830cde748f160e41e7ad889d0f
Maksim <maksalmak@gmail.com>
2017-11-08 15:04:34 +0300

    Начал комментирование кода

--- 6 11/Server/Program.cs
+++ 6 11/Server/Program.cs
@@ -2,14 +2,21 @@
 
 namespace Server
 {
+    //программа сервера
     class Program
     {
+        //Точка входа в программу. Код внутри метода Main будет выполнен этой программой
         static void Main(string[] args)
         {
+            //объект core(ядро) будет управлять работой сервера, принимать сообщения, управлять пользователями и тд
             var core = new ServerCore();
+            //ядро работает параллельно с основной программой, для начала работы надо вызвать метод StartWork()
             core.StartWork();
-            System.Console.WriteLine("STARTED!");
+            //Немного логирования чтобы понять, что сервер запустился
+            Console.WriteLine("STARTED!");
+            //Если в консоли сервера мы нежмем Enter то выполнится метод ReadLine()
             Console.ReadLine();
+            //И мы остановим работу сервера
             core.Stop();
         }
     }
